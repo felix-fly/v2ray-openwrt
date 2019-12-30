5464e604f839cf6ce85c2fa0ebf2c30539a433c8
felix-fly <fenxuehaiyan@qq.com>
2019-12-30 23:42:37 +0800

    Add softfloat.

--- build.sh
+++ build.sh
@@ -18,6 +18,9 @@ build_v2() {
 	echo ">>> Compile v2ray ..."
 	cd main
 	env CGO_ENABLED=0 go build -o $TMP/v2ray${EXESUFFIX} -ldflags "-s -w"
+	if [[ $GOARCH == "mips" || $GOARCH == "mipsle" ]];then
+		env CGO_ENABLED=0 GOMIPS=softfloat go build -o $TMP/v2ray_softfloat -ldflags "-s -w"
+	fi
 	if [[ $GOOS == "windows" ]];then
 	  env CGO_ENABLED=0 go build -o $TMP/wv2ray${EXESUFFIX} -ldflags "-s -w -H windowsgui"
 	fi
@@ -35,6 +38,7 @@ packzip() {
 
 GOOS=$1
 GOARCH=$2
+GOMIPS=
 
 if [ "$1" = "windows" ]; then
   EXESUFFIX=.exe
